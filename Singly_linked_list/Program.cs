17eb13a67fdde492903351fd53ae2bc3cfd17ea4
Alif Nur Rachman <alif.nur.ft21@mail.umy.ac.id>
2022-11-11 17:12:46 +0700

    Add project files.

---
+++ Singly_linked_list/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Singly_linked_list
+{
+    // each node consist of the information part and link to the next mode
+
+    class Node
+    {
+        public int rollNumber;
+        public string name;
+        public Node next;
+
+    }
+
+
+}
